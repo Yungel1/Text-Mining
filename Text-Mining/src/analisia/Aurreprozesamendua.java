b09d1051175258cd63790629365de8de3bd0d3b5
midoiaga <midoiaga@gmail.com>
2021-04-02 01:13:17 +0200

    testutik .arff-era

--- Text-Mining/src/analisia/Aurreprozesamendua.java
+++ Text-Mining/src/analisia/Aurreprozesamendua.java
@@ -1,5 +1,61 @@
 package analisia;
 
-public class Aurreprozesamendua {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Aurreprozesamendua {
+	
+	public void arffFitxategia() throws IOException {
+		
+		FileWriter fw = new FileWriter(new File("/home/mikel/Desktop/Proiektua/mailDatuak.arff"));
+		PrintWriter pw = new PrintWriter(fw);
+		
+		pw.println("@relation mail.data");
+		pw.println("@attribute class {'ham','spam'}");
+		pw.println("@attribute Text string");
+		pw.println("@data");
+		
+		this.textToArff("/home/mikel/Desktop/Downloads/mail_spam/mail_spam/spam/", pw);
+		this.textToArff("/home/mikel/Desktop/Downloads/mail_spam/mail_spam/ham/", pw);
+		
+		pw.close();
+		
+	}
+	
+	
+	public void textToArff(String pathData, PrintWriter pw ) throws IOException {
+		File folder = new File(pathData);
+		String[] zatiak = pathData.split("/");
+		String klasea = zatiak[zatiak.length-1];
+		File[] listOfFiles = folder.listFiles();
+		Scanner myReader;
+		String unekoText="";
+		for (File file : listOfFiles) {
+	    	
+		    if (file.isFile()) {
+		    	myReader = new Scanner(file);
+		        while (myReader.hasNextLine()) {
+		          String data = myReader.nextLine();
+		          unekoText=unekoText.concat(data);
+		        }
+		        pw.println("'"+klasea+"'"+",'"+unekoText+"'");
+		        unekoText="";
+		     
+		    }
+		}
+		//FileWriter fw = new FileWriter(new File(pPath));
+		//PrintWriter pw = new PrintWriter(fw);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		Aurreprozesamendua pro = new Aurreprozesamendua();
+		pro.arffFitxategia();
+		
+		
+	}
+	
 }
