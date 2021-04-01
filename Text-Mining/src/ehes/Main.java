ce7de215ecd88e8c4bb75102819f473aa2029388
Yungel1 <adriansanchez0123@gmail.com>
2021-04-01 19:42:55 +0200

    update

--- Text-Mining/src/ehes/Main.java
+++ Text-Mining/src/ehes/Main.java
@@ -1,9 +1,15 @@
 package ehes;
 
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+		DataSource source = new DataSource(args[0]);
+        Instances data = source.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1);
 
 	}
 
