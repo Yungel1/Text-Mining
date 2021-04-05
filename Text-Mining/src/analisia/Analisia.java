8dc5d682d2dc53fdf44ea660a048a357c1a238eb
Ander <andereiros@gmial.com>
2021-04-05 17:11:20 +0200

    SVM entrenato y pruebas de ekorketa

--- Text-Mining/src/analisia/Analisia.java
+++ Text-Mining/src/analisia/Analisia.java
@@ -1,8 +1,11 @@
 package analisia;
 
 import java.io.IOException;
+import java.util.Random;
 
 import weka.attributeSelection.ClassifierAttributeEval;
+import weka.classifiers.evaluation.Evaluation;
+import weka.classifiers.functions.SMO;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
@@ -22,7 +25,7 @@ public class Analisia {
             System.out.println("\ntrain .arff-a sartu, test .arff sartu, train bektorizatuta gordetzeko path-a\n");
             System.exit(1);
         }*/
-		Analisia analisia = new Analisia();
+		/*Analisia analisia = new Analisia();
 		
 		Instances train = analisia.instantziakKargatu("/home/adrian/EHES/Proiektua/ReutersCorn-train.arff");
 		Instances test = analisia.instantziakKargatu("/home/adrian/EHES/Proiektua/ReutersCorn-test.arff");
@@ -37,6 +40,18 @@ public class Analisia {
 		System.out.println(trainASBektore.numAttributes()+"##"+trainASBektore.classIndex()+"##"+trainASBektore.classAttribute().name());
 		Instances testEgokituta = pro.testaEgokitu("/home/adrian/EHES/Proiektua/ReutersCorn-trainBektore.arff", test);
 		System.out.println(testEgokituta.numAttributes());
+		*/
+		//ANDER
+		Sailkatzailea svm = new Sailkatzailea();
+		DataSource source = new DataSource(args[0]);
+		Instances data = source.getDataSet();
+		data.setClassIndex(data.numAttributes()-1);
+		double[] param = svm.ekortuSVM(data);
+		System.out.println(param[0]+" "+ param[1]);
+		SMO sailk = svm.entrenatuSVM(data, param[0], param[1]);
+		Evaluation e = svm.ebaluatuCrossVal(data, sailk, 10, new Random(1));
+		System.out.println(e.toSummaryString());
+		
 		
 	}
 
--- Text-Mining/src/analisia/Sailkatzailea.java
+++ Text-Mining/src/analisia/Sailkatzailea.java
@@ -1,11 +1,17 @@
 package analisia;
 
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Random;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.functions.Logistic;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.functions.supportVector.Kernel;
+import weka.classifiers.functions.supportVector.RBFKernel;
 import weka.core.Instances;
+import weka.core.Option;
 
 public class Sailkatzailea {
 
@@ -29,4 +35,68 @@ public class Sailkatzailea {
 		return e;
 		
 	}
+	
+	public double[] ekortuSVM(Instances data) throws Exception {
+		
+		SMO svm = new SMO();
+		svm.buildClassifier(data);
+		RBFKernel kernel = new RBFKernel();
+		double[] emaitza = new double[2];
+		emaitza[0] = -1;
+		emaitza[1] = -1;
+		double C = -1;
+		double gamma = -1;
+		double accuracy = 0.00;
+		int atalase = data.numInstances()/5;
+		System.out.println(data.numInstances());
+		for (int i=1;i<=atalase;i++) {
+			
+			svm.setC(i);
+			//System.out.println("owo no he empezado este round");
+			
+			for (int j=1;j<=10;j++) {
+				
+				kernel.setGamma(j);
+				svm.setKernel(kernel);
+				this.entrenatuSVM(data, i, j);
+				Evaluation e = this.ebaluatuCrossVal(data, svm, 10, new Random(1));
+				if (e.pctCorrect()>=accuracy) {
+					
+					C=i;
+					gamma=j;
+					accuracy=e.pctCorrect();
+					
+				}
+				
+				
+			}
+			//System.out.println("Ekortuado "+i+" veces");
+			if (i%10==0) {
+				
+				System.out.println("llevo "+i+" iteraciones");
+				
+			}
+			
+		}
+		emaitza[0]=C;
+		emaitza[1]=gamma;
+		
+		return emaitza;
+	}
+	
+	public SMO entrenatuSVM(Instances data, double C, double gamma) throws Exception {
+		
+		
+		SMO svm = new SMO();
+		svm.buildClassifier(data);
+		svm.setC(C);
+		RBFKernel kernel = new RBFKernel();
+		kernel.setGamma(gamma);
+		svm.setKernel(kernel);
+		return svm;
+		
+	}
+	
+	
+	
 }
