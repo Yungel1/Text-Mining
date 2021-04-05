d75c994fb87ea9fea9d232b795e7f3cfa711ae19
Yungel1 <adriansanchez0123@gmail.com>
2021-04-05 16:47:49 +0200

    interfazea ia eginda

--- Text-Mining/src/ehes/Controller.java
+++ Text-Mining/src/ehes/Controller.java
@@ -10,6 +10,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import weka.classifiers.Classifier;
+import weka.classifiers.evaluation.Evaluation;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class Controller implements Initializable{
 
@@ -23,28 +27,42 @@ public class Controller implements Initializable{
     private TextField txtResult;
 
     @FXML
-    private TextField txtSpam;
-
-    @FXML
-    private TextField txtNoSpam;
-
-    @FXML
     private FontAwesomeIconView iconExit;
 
+    private Instances test;
+    
+    private Classifier cls;
+    
     @FXML
-    void onClick(ActionEvent event) {
+    void onClick(ActionEvent event) throws Exception {
 
+    	//txtDetector --> .txt
+    	//Aurreprozesamendua.java egin eta hau erabiliz .txt --> .arff bihurtu eta FixedDictionary aplikatu
+    	iragarpenaEgin();
     }
 
     @FXML
     void onClickClose(MouseEvent event) {
     	System.exit(0);
     }
+    
+    private void iragarpenaEgin() throws Exception {
+    	
+    	 cls = (Classifier) weka.core.SerializationHelper.read("resources/spam.model");
+    	 
+    	 DataSource source = new DataSource("resources/header.arff");
+         Instances header = source.getDataSet();
+         header.setClassIndex(header.numAttributes() - 1);
+         
+         double pred = cls.classifyInstance(test.instance(0));
+         txtResult.setText(test.classAttribute().value((int) pred));
+    	 
+    }
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 		
+		txtDetector.setPromptText("Paste here the text.");
 	}
 
 }
